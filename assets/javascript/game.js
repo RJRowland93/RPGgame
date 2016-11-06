4727fe00ac2f1f1272cec3113734851f37134c84
RJRowland93 <r.rowland93@gmail.com>
2016-11-06 17:07:35 -0600

    started psuedo coding the game and logic

---
+++ assets/javascript/game.js
@@ -0,0 +1,45 @@
+//============================================
+			//DOM
+//============================================
+	//***theme***
+//character selection
+	//hover over stats
+		//--CSS property :hover
+//win/lose counter
+//chosen character //enemy character
+	//***animation on click?
+//display info section
+	//damage done, damage taken, health left
+//attack button	//reset button
+//============================================
+			//Javascript
+//============================================
+	//global variables
+//============================================
+//character objects 
+	//name, AP (doubles every turn), HP, defense, chosen/enemy(true/false)
+//win count
+//lose count
+//chosen character
+//enemy character
+//============================================
+	//functions
+//============================================
+//game start
+	//player character and opponent selected
+//game reset
+	//dont reset win/lose count
+	//reset character selection and health
+//attack
+	//player deals damage to enemys hp
+	//player recieves defense damage
+//win condition
+	//all enemies defeated health <= 0
+	//show congrats
+//lose condition
+	//chosen character health <=0
+	//show game over
+//============================================
+	//logic
+//============================================
+//game start
