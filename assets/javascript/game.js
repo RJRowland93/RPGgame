f23a59ee27d82425352ca5dab65f5296811d20ca
RJRowland93 <r.rowland93@gmail.com>
2016-11-08 18:43:26 -0600

    have functioning game with updated style and dynamic character interface.

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -8,28 +8,28 @@
 var characters = {
 	rick: {
 		name: "Rick",
-		HP: 0,
-		AP: 0,
-		defense: 0,
-		pic: 0,
+		HP: 100,
+		AP: 5,
+		def: 5,
+		pic: 0
 	},
 	meatwad: {
 		name: "Meatwad",
-		HP: 0,
-		AP: 0,
-		defense:0,
+		HP: 100,
+		AP: 10,
+		def:10
 	},
 	peter: {
 		name: "Peter Griffin",
-		HP: 0,
-		AP: 0,
-		defense:0,
+		HP: 200,
+		AP: 2,
+		def: 4
 	},
 	zoidberg: {
 		name: "Zoidberg",
-		HP: 0,
-		AP: 0,
-		defense: 0,
+		HP: 300,
+		AP: 3,
+		def: 5
 	}
 };
 
@@ -42,10 +42,9 @@ var heroChosen = false;
 var enemyChosen = false;
 var heroAP;
 var heroHP;
-var heroDefense;
-var enemyAP;
 var enemyHP;
-var enemyDefense;
+var enemyDef;
+var grindAP;
 
 //============================================
 	//functions
@@ -59,39 +58,13 @@ defeated = 0;
 heroChosen = false;
 enemyChosen = false;
 heroAP;
+grindAP;
 heroHP;
-heroDefense;
+heroDef;
 enemyAP;
 enemyHP;
-enemyDefense;
+enemyDef;
 	})
-};
-//attack
-function attack() {
-	$("#attack").click(function(){	
-	if (heroChosen && enemyChosen){
-	//hero deals damage to enemys hp
-	enemyHP = enemyHP - heroAP;
-	//hero recieves defense damage
-	heroHP = heroHP - enemyDef;
-	//double heros AP
-	heroAP += heroAP;
-	//if health <=0 add class display:none
-	if (enemyHP <= 0) {
-		$(".enemy").addClass("defeated");
-		defeated++;
-		enemyChosen = false;
-		};
-	if (heroHP <=0) {
-		$(".hero").addClass("defeated");
-			};
-		};
-	});
-};
-	
-
-function combatLog(){
-	$("#combatLog").html(heroName + "<p> has dealt " + heroAP + " damage!</p>\n" + enemyName + "<p> has dealt " + enemyAP + " damage!</p>\n" + heroName + "<p> has " + heroHP + " health remaining.</p>");
 };
 
 function gameOver(){
@@ -99,23 +72,44 @@ function gameOver(){
 	//all enemies defeated health <= 0
 	if (defeated === 3 && heroHP > 0) {
 	//show congrats
-	$("#combatLog").html("<p>Congratulations! You have defeated all of the enemies. You are victorious!</p>");
 	winCount++;
+	$("#info").html("<p>Congratulations! You have defeated all of the enemies. You are victorious!</p>");
+	$("#wins").text(winCount);
 	}
 //lose condition
 	//hero character health <=0
 	if (heroHP <= 0) {
 	//show game over
-	$("#combatLog").html("<p>Defeat has befallen you! You have failed...</p>");
 	loseCount++;
+	$("#info").html("<p>Defeat has befallen you! You have failed...</p>");
+	$("#losses").text(loseCount);
 	}
 }; 
 
 function charGen() {
-	$(".peter").data("stats", {"name":"peter", "HP": 400, "AP": 50, "def": 25});
-	$(".rick").data("stats", {"name":"rick", "HP": 200, "AP": 100, "def": 50});
-	$(".zoidberg").data("stats", {"name":"zoidberg", "HP": 300, "AP": 75, "def": 35});
-	$(".meatwad").data("stats", {"name":"meatwad", "HP": 1000, "AP": 10, "def": 10});
+	$(".peter").data("stats", characters.peter);
+	$(".rick").data("stats", characters.rick);
+	$(".zoidberg").data("stats", characters.zoidberg);
+	$(".meatwad").data("stats", characters.meatwad);
+};
+
+function statDisplay() {
+	$("#meatwadName").text(characters.meatwad.name);
+	$("#meatwadHP").text("Health: "+characters.meatwad.HP);
+	$("#meatwadAP").text("Attack: "+characters.meatwad.AP);
+	$("#meatwadDef").text("Defense: "+characters.meatwad.def);
+	$("#peterName").text(characters.peter.name);
+	$("#peterHP").text("Health: "+characters.peter.HP);
+	$("#peterAP").text("Attack: "+characters.peter.AP);
+	$("#peterDef").text("Defense: "+characters.peter.def);
+	$("#rickName").text(characters.rick.name);
+	$("#rickHP").text("Health: "+characters.rick.HP);
+	$("#rickAP").text("Attack: "+characters.rick.AP);
+	$("#rickDef").text("Defense: "+characters.rick.def);
+	$("#zoidbergName").text(characters.zoidberg.name);
+	$("#zoidbergHP").text("Health: "+characters.zoidberg.HP);
+	$("#zoidbergAP").text("Attack: "+characters.zoidberg.AP);
+	$("#zoidbergDef").text("Defense: "+characters.zoidberg.def);
 };
 
 //============================================
@@ -125,40 +119,63 @@ function charGen() {
 $(document).ready(function(){
 	//create character stats
 	charGen();
+	statDisplay();
+
 	//choose hero
 $(".character").on("dblclick", function() {
-	if (heroChosen == false){
+	if (heroChosen === false){
 	$( this ).addClass("hero");
 	$( this ).appendTo("#hero");
 	heroChosen = true;
 	heroName = $( this ).data("stats").name;
 	heroAP = $( this ).data("stats").AP;
+	grindAP = $( this ).data("stats").AP;
 	heroHP = $( this ).data("stats").HP;
-	heroDef = $( this ).data("stats").def;
 //choose enemy
-} else if (enemyChosen == false) {
+} else if (enemyChosen === false) {
 	$( this ).addClass("enemy");
 	$( this ).appendTo("#enemy");
 	enemyChosen = true;
 	enemyName = $( this ).data("stats").name;
-	enemyAP = $( this ).data("stats").AP;
 	enemyHP = $( this ).data("stats").HP;
 	enemyDef = $( this ).data("stats").def;
-	}
+	};
 });
 
+//attack rounds
 $("#attack").on("click", function() {
-//attack button only enabled if both hero and enemy selected
-	attack();
-//show combat log
-	combatLog();
-//run win/lose conditions on each attack click
+if (heroChosen && enemyChosen){
+	//hero deals damage to enemys hp
+	enemyHP -= heroAP;
+	//hero recieves defense damage
+	heroHP -= enemyDef;
+	//double heros AP
+	heroAP += grindAP;
+	//show changes in stats
+	$(".hero .topRight").text("Health: "+heroHP);
+	$(".hero .botLeft").text("Attack: "+heroAP);
+	$(".enemy .topRight").text("Health: "+heroHP);
+	//if health <=0 add class display:none
+	if (enemyHP <= 0) {
+		$(".enemy").addClass("defeated");
+		defeated++;
+		enemyChosen = false;
+		};
+	if (heroHP <=0) {
+		$(".hero").addClass("defeated");
+		};
+		console.log(heroHP);
+		console.log(heroAP);
+		console.log(grindAP);
+		console.log(enemyHP);
+		console.log(enemyDef);
+	};
 	gameOver();
 });
-
-$("#reset").on("click", function() {
-	reset();
-	});
+//reset button
+	$("#reset").on("click", reset);
 });
+
+
 
 
